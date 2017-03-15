2fd4abf434351e3d63bbea1fb1bd2990e2f8dd05
HP <rey880603@gmail.com>
2017-03-15 00:06:26 -0600

    terminadas las interfacez

--- src/AppBundle/Controller/UserController.php
+++ src/AppBundle/Controller/UserController.php
@@ -14,48 +14,48 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;use Symfony\Component
  */
 class UserController extends Controller
 {
-    /**
-     * Lists all user entities.
-     *
-     * @Route("/", name="user_index")
-     * @Method("GET")
-     */
-    public function indexAction()
-    {
-        $em = $this->getDoctrine()->getManager();
+//    /**
+//     * Lists all user entities.
+//     *
+//     * @Route("/", name="user_index")
+//     * @Method("GET")
+//     */
+//    public function indexAction()
+//    {
+//        $em = $this->getDoctrine()->getManager();
+//
+//        $users = $em->getRepository('AppBundle:User')->findAll();
+//
+//        return $this->render('user/index.html.twig', array(
+//            'users' => $users,
+//        ));
+//    }
 
-        $users = $em->getRepository('AppBundle:User')->findAll();
-
-        return $this->render('user/index.html.twig', array(
-            'users' => $users,
-        ));
-    }
-
-    /**
-     * Creates a new user entity.
-     *
-     * @Route("/new", name="user_new")
-     * @Method({"GET", "POST"})
-     */
-    public function newAction(Request $request)
-    {
-        $user = new User();
-        $form = $this->createForm('AppBundle\Form\UserType', $user);
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            $em = $this->getDoctrine()->getManager();
-            $em->persist($user);
-            $em->flush($user);
-
-            return $this->redirectToRoute('user_show', array('id' => $user->getId()));
-        }
-
-        return $this->render('user/new.html.twig', array(
-            'user' => $user,
-            'form' => $form->createView(),
-        ));
-    }
+//    /**
+//     * Creates a new user entity.
+//     *
+//     * @Route("/new", name="user_new")
+//     * @Method({"GET", "POST"})
+//     */
+//    public function newAction(Request $request)
+//    {
+//        $user = new User();
+//        $form = $this->createForm('AppBundle\Form\UserType', $user);
+//        $form->handleRequest($request);
+//
+//        if ($form->isSubmitted() && $form->isValid()) {
+//            $em = $this->getDoctrine()->getManager();
+//            $em->persist($user);
+//            $em->flush($user);
+//
+//            return $this->redirectToRoute('user_show', array('id' => $user->getId()));
+//        }
+//
+//        return $this->render('user/new.html.twig', array(
+//            'user' => $user,
+//            'form' => $form->createView(),
+//        ));
+//    }
 
     /**
      * Finds and displays a user entity.
@@ -88,7 +88,8 @@ class UserController extends Controller
         if ($editForm->isSubmitted() && $editForm->isValid()) {
             $this->getDoctrine()->getManager()->flush();
 
-            return $this->redirectToRoute('user_edit', array('id' => $user->getId()));
+//            return $this->redirectToRoute('user_edit', array('id' => $user->getId()));
+            return $this->redirectToRoute('homepage');
         }
 
         return $this->render('user/edit.html.twig', array(
@@ -101,21 +102,21 @@ class UserController extends Controller
     /**
      * Deletes a user entity.
      *
-     * @Route("/{id}", name="user_delete")
-     * @Method("DELETE")
+     * @Route("/delete/{id}", name="user_delete")
      */
-    public function deleteAction(Request $request, User $user)
+    public function deleteAction($id)
     {
-        $form = $this->createDeleteForm($user);
-        $form->handleRequest($request);
+        $em = $this->getDoctrine()->getManager();
+        $entity = $em->getRepository('AppBundle:User')->find($id);
 
-        if ($form->isSubmitted() && $form->isValid()) {
-            $em = $this->getDoctrine()->getManager();
-            $em->remove($user);
-            $em->flush();
+        if (!$entity) {
+            throw $this->createNotFoundException('Unable to find Familiar entity.');
         }
 
-        return $this->redirectToRoute('user_index');
+        $em->remove($entity);
+        $em->flush();
+
+        return $this->redirectToRoute('homepage');
     }
 
     /**
