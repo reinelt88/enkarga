750c30ff0059a95e1f8045bf486124e098ad1132
HP <rey880603@gmail.com>
2017-03-14 20:50:15 -0600

    entity create start to template...

--- src/AppBundle/Entity/User.php
+++ src/AppBundle/Entity/User.php
@@ -19,9 +19,254 @@ class User extends BaseUser
      */
     protected $id;
 
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="name", type="string", length=30, nullable=false)
+     */
+    private $name;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="lastname", type="string", length=30, nullable=false)
+     */
+    private $lastname;
+
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="postal_code", type="integer", length=5, nullable=false)
+     */
+    private $postalCode;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="entity", type="string", length=100, nullable=false)
+     */
+    private $entity;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="city", type="string", length=100, nullable=false)
+     */
+    private $city;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="settlement", type="string", length=100, nullable=false)
+     */
+    private $settlement;
+
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="street", type="string", length=100, nullable=false)
+     */
+    private $street;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="number", type="string", length=10, nullable=false)
+     */
+    private $number;
+
+
+
     public function __construct()
     {
         parent::__construct();
         // your own logic
+    }
+
+
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     * @return User
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string 
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set lastname
+     *
+     * @param string $lastname
+     * @return User
+     */
+    public function setLastname($lastname)
+    {
+        $this->lastname = $lastname;
+
+        return $this;
+    }
+
+    /**
+     * Get lastname
+     *
+     * @return string 
+     */
+    public function getLastname()
+    {
+        return $this->lastname;
+    }
+
+    /**
+     * Set postalCode
+     *
+     * @param integer $postalCode
+     * @return User
+     */
+    public function setPostalCode($postalCode)
+    {
+        $this->postalCode = $postalCode;
+
+        return $this;
+    }
+
+    /**
+     * Get postalCode
+     *
+     * @return integer 
+     */
+    public function getPostalCode()
+    {
+        return $this->postalCode;
+    }
+
+    /**
+     * Set entity
+     *
+     * @param string $entity
+     * @return User
+     */
+    public function setEntity($entity)
+    {
+        $this->entity = $entity;
+
+        return $this;
+    }
+
+    /**
+     * Get entity
+     *
+     * @return string 
+     */
+    public function getEntity()
+    {
+        return $this->entity;
+    }
+
+    /**
+     * Set street
+     *
+     * @param string $street
+     * @return User
+     */
+    public function setStreet($street)
+    {
+        $this->street = $street;
+
+        return $this;
+    }
+
+    /**
+     * Get street
+     *
+     * @return string 
+     */
+    public function getStreet()
+    {
+        return $this->street;
+    }
+
+    /**
+     * Set number
+     *
+     * @param string $number
+     * @return User
+     */
+    public function setNumber($number)
+    {
+        $this->number = $number;
+
+        return $this;
+    }
+
+    /**
+     * Get number
+     *
+     * @return string 
+     */
+    public function getNumber()
+    {
+        return $this->number;
+    }
+
+    /**
+     * Set city
+     *
+     * @param string $city
+     * @return User
+     */
+    public function setCity($city)
+    {
+        $this->city = $city;
+
+        return $this;
+    }
+
+    /**
+     * Get city
+     *
+     * @return string 
+     */
+    public function getCity()
+    {
+        return $this->city;
+    }
+
+    /**
+     * Set settlement
+     *
+     * @param string $settlement
+     * @return User
+     */
+    public function setSettlement($settlement)
+    {
+        $this->settlement = $settlement;
+
+        return $this;
+    }
+
+    /**
+     * Get settlement
+     *
+     * @return string 
+     */
+    public function getSettlement()
+    {
+        return $this->settlement;
     }
 }
